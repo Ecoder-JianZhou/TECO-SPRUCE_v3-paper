3e5857b4e526767071d13064a6cf90b702a96d4d
Ecoder-JianZhou <ecoder-zj@outlook.com>
2024-04-03 02:18:24 -0400

    update

--- run_scripts/pretreat/make_desktop_all.sh
+++ run_scripts/pretreat/make_desktop_all.sh
@@ -67,6 +67,37 @@ fi
 
 
 
+gfortran  -g -fbacktrace -Wall -fcheck=all -cpp -DUSE_NETCDF\
+    src/teco/datatypes.f90 \
+    src/teco/io_mod.f90 \
+    src/teco/soil.f90 \
+    src/teco/vegetation.f90 \
+    src/teco/transfer.f90 \
+    src/tools/mcmc_mod.f90 \
+    src/teco/driver.f90 \
+    src/tools/mcmc_outputs.f90  \
+    src/tools/mcmc_2011-2013/mcmc_P07.f90   \
+    src/tools/sensitivity.f90   \
+    src/tools/spinup.f90  \
+    src/main.f90 \
+    -o run_teco_desktop_P07 \
+    -I/home/jz964/miniconda3/envs/spruce-da/include -L/home/jz964/miniconda3/envs/spruce-da/lib -lnetcdff -lnetcdf
+
+if find "src" -name "*.mod" -print -quit | grep -q '.*'; then
+    rm src/*.mod
+fi
+
+if find "src/teco" -name "*.mod" -print -quit | grep -q '.*'; then
+    rm src/teco/*.mod
+fi
+
+if find "src/tools" -name "*.mod" -print -quit | grep -q '.*'; then
+    rm src/tools/*.mod
+fi
+
+if find "." -name "*.mod" -print -quit | grep -q '.*'; then
+    rm *.mod
+fi
 
 gfortran  -g -fbacktrace -Wall -fcheck=all -cpp -DUSE_NETCDF\
     src/teco/datatypes.f90 \
